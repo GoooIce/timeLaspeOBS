e92969c70e934bf58a8a88f61fd4c4936069fe13
王雪 <broken.ice1314@gmail.com>
2022-09-20 16:29:37 +0800

    chore: module

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use libobs_sys::{obs_module_t, LIBOBS_API_MAJOR_VER, LIBOBS_API_MINOR_VER, LIBOB
 use libobs_wrapper::log::Logger;
 use log::info;
 
-// static mut OBS_MODULE: Option<$t> = None;
 pub static mut OBS_MODULE_POINTER: Option<*mut obs_module_t> = None;
 
 #[no_mangle]
@@ -19,8 +18,13 @@ pub unsafe extern "C" fn obs_module_unload() {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn obs_module_set_pointer(raw: *mut obs_module_t) {
-    OBS_MODULE_POINTER = Some(raw)
+pub unsafe extern "C" fn obs_module_set_pointer(module: *mut obs_module_t) {
+    OBS_MODULE_POINTER = Some(module)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn obs_current_module() -> *mut obs_module_t {
+    OBS_MODULE_POINTER.unwrap()
 }
 
 #[no_mangle]
